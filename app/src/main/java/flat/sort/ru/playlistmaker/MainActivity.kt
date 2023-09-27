5856e49ed22f2d448a82f65039258b566c9ff04d
Sboev Andrey <a.sboev@teleswyz.ru>
2023-09-27 19:08:32 +0300

    activity added

--- app/src/main/java/flat/sort/ru/playlistmaker/MainActivity.kt
+++ app/src/main/java/flat/sort/ru/playlistmaker/MainActivity.kt
@@ -1,11 +1,35 @@
 package flat.sort.ru.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val searchButton = findViewById<Button>(R.id.search_btn)
+        val libraryButton = findViewById<Button>(R.id.library_btn)
+        val settingButton = findViewById<Button>(R.id.settings_btn)
+        val searchBtnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                //Toast.makeText(this@MainActivity, getString(R.string.main_search_btn_toast_text), Toast.LENGTH_SHORT).show()
+                val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(searchIntent)
+            }
+        }
+        searchButton.setOnClickListener(searchBtnClickListener)
+        libraryButton.setOnClickListener {
+            //Toast.makeText(this@MainActivity, getString(R.string.main_library_btn_toast_text), Toast.LENGTH_SHORT).show()
+            val libraryIntent = Intent(this@MainActivity, LibraryActivity::class.java)
+            startActivity(libraryIntent)
+        }
+        settingButton.setOnClickListener {
+            //Toast.makeText(this@MainActivity, getString(R.string.main_settings_btn_toast_text), Toast.LENGTH_SHORT).show()
+            val settingsIntent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
     }
 }
---
+++ app/src/main/java/flat/sort/ru/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,19 @@
+package flat.sort.ru.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<ImageView>(R.id.arrow_back)
+        backButton.setOnClickListener{
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
+    }
+}
