7aa74d4f308ed82e4b41f0577417ce3a70288d41
Sboev Andrey <a.sboev@teleswyz.ru>
2023-10-09 18:43:45 +0300

    SettingsActivity buttons

--- app/src/main/java/flat/sort/ru/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/flat/sort/ru/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package flat.sort.ru.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,6 +15,31 @@ class SettingsActivity : AppCompatActivity() {
         val backButton = findViewById<ImageView>(R.id.arrow_back)
         backButton.setOnClickListener {
             super.onBackPressed()
+        }
+
+        val shareButton = findViewById<FrameLayout>(R.id.share_button)
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.extra_text_for_send))
+            startActivity(intent)
+        }
+
+        val supportButton = findViewById<FrameLayout>(R.id.write_to_support_button)
+        supportButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.extra_mail)))
+            intent.putExtra(Intent.EXTRA_SUBJECT,  getString(R.string.extra_subject))
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.extra_text))
+            startActivity(intent)
+        }
+
+        val termsButton = findViewById<FrameLayout>(R.id.terms_of_use_button)
+        termsButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(getString(R.string.practicum_offer_uri))
+            startActivity(intent)
         }
     }
 }
