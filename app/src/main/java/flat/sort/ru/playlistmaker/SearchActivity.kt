54a361ece50eb9774c3acaf56f1932685760a2a8
Sboev Andrey <a.sboev@teleswyz.ru>
2023-10-14 10:31:46 +0300

    SettingsActivity dev in progress

--- app/src/main/java/flat/sort/ru/playlistmaker/SearchActivity.kt
+++ app/src/main/java/flat/sort/ru/playlistmaker/SearchActivity.kt
@@ -2,10 +2,30 @@ package flat.sort.ru.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        }
+
+        val searchEditText = findViewById<EditText>(R.id.search_edit_text)
+        searchEditText.addTextChangedListener(textWatcher)
     }
 }
